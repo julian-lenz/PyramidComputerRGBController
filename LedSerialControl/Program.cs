742ce09815ac7c2638386e1f735ffda11d264093
JulianLenzenweger <fhs48109@fh-salzburg.ac.at>
2023-12-07 15:53:14 +0100

    Testing all Functions

--- LedSerialControl/Program.cs
+++ LedSerialControl/Program.cs
@@ -7,8 +7,61 @@ class Program
 {
 
     static void Main()
-    {
-       var rgbController = new LedSerialControl.RGBController("COM4");
-       rgbController.StartFlashing();
+    { 
+        const int fadetime = 1; 
+        var rgbController = new LedSerialControl.RGBController("COM4");
+        rgbController.StopFlashing();
+        rgbController.SetColorRgbw(0, 0, 0, 0);
+        Thread.Sleep(500);
+        rgbController.SetFlashingColors(RGBController.ColorNames.Yellow, RGBController.ColorNames.White);
+        rgbController.SetFlashingPeriod(30);
+        rgbController.StartFlashing();
+        Thread.Sleep(6000);
+        rgbController.SetFlashingPeriod(10);
+        Thread.Sleep(3000);
+        rgbController.StopFlashing();
+        for (int i = 0; i < 2; i++)
+        {
+            // Start with red
+            byte r = 255, g = 0, b = 0;
+           
+            // Fade from red to blue
+            for (; b < 255; b++)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+            for (; r > 0; r--)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+            // Increase green to 255
+            for (; g < 255; g++)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+            for (; b > 0; b--)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+            // Increase red to 255
+            for (; r < 255; r++)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+            for (; g > 0; g--)
+            {
+                rgbController.SetColorRgbw(r, g, b, 0);
+                Thread.Sleep(fadetime);
+            }
+        }
+
+        rgbController.SetColor(RGBController.ColorNames.Off);
+
+
     }
 }
